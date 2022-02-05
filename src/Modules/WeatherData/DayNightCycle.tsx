bb680ae52c7a767ef58b236fec77acd11b174660
4RCZ1 <marcinowicz1@gmail.com>
2022-02-05 16:39:46 +0100

    fixed scaling issue

--- src/Modules/WeatherData/DayNightCycle.tsx
+++ src/Modules/WeatherData/DayNightCycle.tsx
@@ -49,7 +49,7 @@ const DayNightCycle = ({sunrise, sunset, localtime_epoch, timeDifference, moonPh
     const currentDateString = new Date(currentDate).toLocaleTimeString().slice(0, -3);
 
     const currentWidth = useCurrentWidth();
-    const canvasWidth = currentWidth > 768 ? (currentWidth - 60) * 0.9 : (currentWidth - 60) * 0.98;
+    const canvasWidth = currentWidth > 768 ? (currentWidth - 50) * 0.9 : (currentWidth - 50) * 0.98;
 
     let SunX: number | null = null;
     const setSunX = (val: number) => {
